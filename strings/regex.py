9fc014ea0750cfbaf8ce4b61b287dd01b4100bb8
Yassunaga <gabriel_yassunaga@hotmail.com>
2020-05-22 11:12:08 -0300

    Melhorias na regex

--- strings/regex.py
+++ strings/regex.py
@@ -5,7 +5,7 @@ email2 = "Fale comigo em 1234-1234 esse é meu telefone"
 email3 = "1234-1234 é meu celular"
 email4 = "lalalalalla 993540-12531 iasdawjenmae"
 
-padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]"
+padrao = "[0-9]{4}[-][0-9]{4}"
 
 retorno = re.search(padrao, email4)
 print(retorno.group())
