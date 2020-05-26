6eeb6b6e905214bbf66a93095a88f200a70fc6d7
Yassunaga <gabriel_yassunaga@hotmail.com>
2020-05-26 12:27:40 -0300

    Atributos dinâmicos na página

--- flask_1/jogoteca/jogoteca.py
+++ flask_1/jogoteca/jogoteca.py
@@ -1,16 +1,24 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
+
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
 
 
 @app.route("/")
 def ola():
-    lista = ['Tetris', 'Super Mario', 'Pokemon Gold']
+    jogo1 = Jogo("Super Mario", "Plataforma", "SNES")
+    jogo2 = Jogo("Pokemon Gold", "RPG", "GameBoy")
+    jogo3 = Jogo("Mortal Kombat", "Luta", "SNES")
+
+    lista = [jogo1, jogo2, jogo3]
     return render_template('index.html', titulo='Jogos',
                            jogos=lista)
 
 
 app.run(host="0.0.0.0", port="5000")
-
-
