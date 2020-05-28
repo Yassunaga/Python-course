da3148dba3e0f9d18bc31bab6589decdf235fe8d
Yassunaga <gabriel_yassunaga@hotmail.com>
2020-05-28 12:58:42 -0300

    Adicionando autenticação

--- flask_1/jogoteca/jogoteca.py
+++ flask_1/jogoteca/jogoteca.py
@@ -1,31 +1,38 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, session, flash
+from loguru import logger
 
 app = Flask(__name__)
+app.secret_key = "ULTRA SECRETO"
 
 
 class Jogo:
+
     def __init__(self, nome, categoria, console):
         self.nome = nome
         self.categoria = categoria
         self.console = console
 
+    def __str__(self):
+        return f"{self.nome}, {self.console}, {self.categoria}"
 
-jogo1 = Jogo("Super Mario", "Plataforma", "SNES")
-jogo2 = Jogo("Pokemon Gold", "RPG", "GameBoy")
-jogo3 = Jogo("Mortal Kombat", "Luta", "SNES")
+
+jogo1 = Jogo("Mario", "Plataforma", "SNES")
+jogo2 = Jogo("Mortal Kombat", "Luta", "SNES")
+jogo3 = Jogo("Rayman", "Plataforma", "Xbox")
 
 lista = [jogo1, jogo2, jogo3]
 
 
 @app.route("/")
 def index():
-    return render_template('index.html', titulo='Jogos',
-                           jogos=lista)
+    logger.info("ABRINDO PÁGINA INICIAL")
+    return render_template('index.html', jogos=lista, titulo="Jogoteca")
 
 
-@app.route("/novo")
+@app.route('/novo')
 def novo():
-    return render_template("novo.html", titulo="Novo Jogo")
+    logger.info("CADASTRANDO NOVO JOGO")
+    return render_template('novo.html')
 
 
 @app.route("/criar", methods=['POST'])
@@ -33,11 +40,33 @@ def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
-    jogo = Jogo(nome, categoria, console)
-    lista.append(jogo)
-    return redirect(url_for("index"))
+
+    novo_jogo = Jogo(nome, categoria, console)
+    lista.append(novo_jogo)
+    logger.info("CADASTRANDO NOVO JOGO")
+    logger.info(f"{nome}")
+
+    return redirect("/")
 
 
-app.run(host="0.0.0.0",
-        port="5000",
-        debug=True)
+@app.route("/autenticar", methods=['POST'])
+def autenticar():
+    usuario = request.form['usuario']
+    senha = request.form['senha']
+    session['usuario_logado'] = usuario
+
+    if senha == 'mestra':
+        flash(f"Usuário {usuario} logado com sucesso!")
+        return redirect("/")
+    else:
+        flash("Usuário ou senha incorretos!")
+        return redirect("/login")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+app.run(host="0.0.0.0", port=5000, debug=True)
+logger.info("INICIANDO APLICAÇÃO")
